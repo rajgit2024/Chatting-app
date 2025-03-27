5cf2138b40fb79282ac4b50aed59e298805c4fdc
raj dubey <rajdu590@gmail.com>
2025-03-27 22:39:36 +0530

    Initial commit with front-end and backend folders

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -10,10 +10,13 @@ router.post("/register",(req,res)=>{
     res.send("register route!")
 })
 
+<<<<<<< HEAD
 router.post("/hello",(req,res)=>{
     res.send("hello route!")
 })
 
+=======
+>>>>>>> 28d0f5a (Initial commit with front-end and backend folders)
 router.post("/logout",(req,res)=>{
     res.send("Logout route!")
 })
